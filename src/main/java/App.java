6160ab3f202d127bfc98d0bced76a2adc2508a4e
KhanyisoCairo <Khanyisoyezo12@gmail.com>
2020-10-01 15:37:55 +0200

    deployed to heroku

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,23 +1,101 @@
+import org.jdbi.v3.core.Jdbi;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
-import static spark.Spark.get;
-import static spark.Spark.staticFileLocation;
+import static spark.Spark.*;
 
 public class App {
+
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+    }
+
+
+    static Jdbi getDatabaseConnection(String defualtJdbcUrl) throws URISyntaxException, SQLException {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        String database_url = processBuilder.environment().get("DATABASE_URL");
+        String local = processBuilder.environment().get("LOCAL");
+        if (local == null && database_url != null) {
+
+            URI uri = new URI(database_url);
+            String[] hostParts = uri.getUserInfo().split(":");
+            String username = hostParts[0];
+            String password = hostParts[1];
+            String host = uri.getHost();
+
+            int port = uri.getPort();
+
+            String path = uri.getPath();
+            String url = String.format("jdbc:postgresql://%s:%s%s", host, port, path);
+
+            return Jdbi.create(url, username, password);
+
+        } else if (local != null && database_url != null) {
+            return Jdbi.create(database_url);
+        }
+
+        return Jdbi.create(defualtJdbcUrl);
+
+    }
+
+
     public static void main(String[] args) {
-        staticFileLocation("/public");
-        staticFileLocation("/templates");
 
-        get("/", (req, res) -> {
+        try {
+            Jdbi jdbi = getDatabaseConnection("jdbc:postgresql://localhost/waiters?user=khanyiso&password=cairo123");
 
-            Map<String, String> dataMap = new HashMap<>();
+            staticFileLocation("/public");
+            staticFileLocation("/templates");
+            port(getHerokuAssignedPort());
+            Waiters waiters = new Waiters(jdbi);
+            get("/", (req, res) -> {
 
-            return new ModelAndView(dataMap, "logIn.handlebars");
-        }, new HandlebarsTemplateEngine());
+                Map<String, String> dataMap = new HashMap<>();
+                res.redirect("login.handlebars");
+                return new ModelAndView(dataMap, "login.handlebars");
+            }, new HandlebarsTemplateEngine());
+
+            get("/waiters/:username", (req, res) -> {
+                waiters.setUser(req.params("username"));
+                waiters.getUser();
+                Map<String, String> dataMap = new HashMap<>();
+
+                return new ModelAndView(dataMap, "login.handlebars");
+            }, new HandlebarsTemplateEngine());
+
+
+//            get("/days", (req, res) -> {
+//
+//                Map<String, String> dataMap = new HashMap<>();
+//
+//                return new ModelAndView(dataMap, ".handlebars");
+//            }, new HandlebarsTemplateEngine());
+
+
+//            post("/waiters/:username", (req, res) -> {
+//
+//                Map<String, String> dataMap = new HashMap<>();
+//
+//                return new ModelAndView(dataMap, "booking.handlebars");
+//            }, new HandlebarsTemplateEngine());
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+
     }
 
 }
--- src/main/java/Waiters.java
+++ src/main/java/Waiters.java
@@ -1,20 +1,51 @@
+import org.jdbi.v3.core.Jdbi;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Waiters {
-    Map<String,Integer> waiters = new HashMap<>();
+    Map<String, Integer> waiters = new HashMap<>();
+    private Jdbi connection;
 
-    public void setUser(String name) {
-        if (waiters.equals(name)) {
-            waiters.put(name,waiters.get(name)+1);
-        } else {
-            waiters.put(name, 1);
-        }
+    public Waiters(Jdbi connection) {
+        this.connection = connection;
     }
 
-    public Map<String,Integer> getUser(){
+    public void setUser(String name) {
+
+        connection.withHandle(handle -> handle.createQuery("INSERT INTO UserName (name) values (?)")
+                .bind(name,1)
+        );
+        //        try {
+//            PreparedStatement ps = connection.prepareStatement("INSERT INTO UserName (name) values (?)");
+//            ps.setString(1, name);
+//            ps.execute();
+//        } catch (SQLException throwables) {
+//            throwables.printStackTrace();
+//        }
+
+    }
+    public Map<String, Integer> getUser() {
+        connection.useHandle(handle -> handle.execute("SELECT * FROM  UserName"));
+//        try {
+//            PreparedStatement ps = connection.prepareStatement("SELECT * FROM  UserName");
+//            ResultSet rs =ps.executeQuery();
+//            while (rs.next()){
+//                System.out.println(rs.getString("name"));
+//                System.out.println(rs.getInt("id"));
+//
+//            }
+//
+//        } catch (SQLException throwables) {
+//            throwables.printStackTrace();
+//        }
+
         return waiters;
     }
 
